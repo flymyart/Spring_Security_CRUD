99f538b95ebc76e5ab7a7783e89812a6aa75af62
Tatiana <43416772+flymyart@users.noreply.github.com>
2023-09-25 13:54:14 +0300

    Update RoleService.java

--- src/main/java/ru/kata/spring/boot_security/demo/services/RoleService.java
+++ src/main/java/ru/kata/spring/boot_security/demo/services/RoleService.java
@@ -1,18 +1,5 @@
 package ru.kata.spring.boot_security.demo.services;
 
-import org.springframework.stereotype.Service;
-import ru.kata.spring.boot_security.demo.repositories.RoleRepository;
-
-@Service
-public class RoleService {
-
-    private RoleRepository roleRepository;
-
-    public RoleService(RoleRepository roleRepository) {
-        this.roleRepository = roleRepository;
-    }
-
-    public Object findAll() {
-        return roleRepository.findAll();
-    }
+public interface RoleService {
+    Object findAll();
 }
