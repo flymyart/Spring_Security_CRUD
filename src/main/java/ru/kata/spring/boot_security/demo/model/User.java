8d09a07170fb1cca80f6ef8ac45403fae17f2d2e
Tatiana <43416772+flymyart@users.noreply.github.com>
2023-09-25 13:02:09 +0300

    Update User.java

--- src/main/java/ru/kata/spring/boot_security/demo/model/User.java
+++ src/main/java/ru/kata/spring/boot_security/demo/model/User.java
@@ -20,6 +20,7 @@ import java.util.List;
 @Entity
 @Table(name = "users")
 public class User implements UserDetails {
+    
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
