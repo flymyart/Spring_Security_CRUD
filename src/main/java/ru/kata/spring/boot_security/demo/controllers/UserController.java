88cf7c1ad174d9e8167b1aea9a964dcf73f63b55
Tatiana <43416772+flymyart@users.noreply.github.com>
2023-09-25 13:01:35 +0300

    Update UserController.java

--- src/main/java/ru/kata/spring/boot_security/demo/controllers/UserController.java
+++ src/main/java/ru/kata/spring/boot_security/demo/controllers/UserController.java
@@ -4,22 +4,23 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import ru.kata.spring.boot_security.demo.services.UserService;
+import ru.kata.spring.boot_security.demo.services.SecurityService;
 
 import java.security.Principal;
 
 @Controller
 @RequestMapping("/user")
 public class UserController {
-    private final UserService userService;
 
-    public UserController(UserService userService) {
-        this.userService = userService;
+    private final SecurityService securityService;
+
+    public UserController(SecurityService securityService) {
+        this.securityService = securityService;
     }
 
     @GetMapping()
     public String show(Principal principal, Model model) {
-        model.addAttribute("user", userService.findByUsername(principal.getName()));
+        model.addAttribute("user", securityService.findByUsername(principal.getName()));
         return "user";
     }
 }
