18eaa01fd7b8881c1ff7e4d7517a51144c3998f2
Tatiana <43416772+flymyart@users.noreply.github.com>
2023-09-25 13:02:00 +0300

    Update Role.java

--- src/main/java/ru/kata/spring/boot_security/demo/model/Role.java
+++ src/main/java/ru/kata/spring/boot_security/demo/model/Role.java
@@ -14,6 +14,7 @@ import java.util.Collection;
 @Entity
 @Table(name = "roles")
 public class Role implements GrantedAuthority {
+    
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
